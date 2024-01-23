93e9ba46d9343ab17022eae7bf9c5e01363df9a4
Soham Datta <sohamdatta34@gmail.com>
2024-01-23 10:47:15 +0530

    fix: form getting submitted on file upload

--- src/components/hook-form/RHFDocUpload.tsx
+++ src/components/hook-form/RHFDocUpload.tsx
@@ -14,7 +14,6 @@ const RHFDocUpload: React.FC<RHFDocUploadProps> = ({ name, label, onFileChange }
 
     const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
         const newFile = event.target.files && event.target.files[0];
-        // const newFile = event.currentTarget.files && event.currentTarget.files[0];
 
         if (newFile) {
             onFileChange(name, newFile);
@@ -39,28 +38,28 @@ const RHFDocUpload: React.FC<RHFDocUploadProps> = ({ name, label, onFileChange }
             control={control}
             name={name}
             render={({ field }) => (
-                <div>
+                <div className="mb-4">
                     <label className="bg-blue-500 text-white px-4 py-2 rounded cursor-pointer">
                         {label}
+                        <input
+                            type="file"
+                            className="hidden"
+                            onChange={(e) => {
+                                field.onChange(e.target.files[0]);
+                                handleFileChange(e);
+                            }}
+                            onBlur={field.onBlur}
+                        />
                     </label>
-                    <input
-                        type="file"
-                        // onChange={(e) => field.onChange(e.target.files[0])}
-                        onChange={(e) => {
-                            field.onChange(e.target.files[0]);
-                            handleFileChange(e);
-                        }}
-                        onBlur={field.onBlur}
-                    />
                     {file && (
-                        <>
-                            <span className="ml-2 cursor-pointer text-blue-500 underline" onClick={onView}>
+                        <div className="mt-2">
+                            <span className="cursor-pointer text-blue-500 underline" onClick={onView}>
                                 View
                             </span>
-                            <span className="ml-2 cursor-pointer text-blue-500 underline" onClick={onRemoveFile}>
+                            <span className="ml-2 cursor-pointer text-red-500 underline" onClick={onRemoveFile}>
                                 Remove
                             </span>
-                        </>
+                        </div>
                     )}
                 </div>
             )}
